ec2779d019a123deee5f4b0a85d17355af5a642c
hqz3 <heqzheng@gmail.com>
2022-05-05 18:38:53 -0400

    Create App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
+// Component
+import Input from "./components/Input";
+import TodoList from "./components/TodoList";
+// Model
+import { Todo } from "./model";
+// Style
 import "./App.css";
 
-function App() {
-  return <div className="App">Hello World</div>;
-}
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([
+        ...todos,
+        {
+          id: Date.now(),
+          todo,
+          isComplete: false,
+        },
+      ]);
+      setTodo("");
+    }
+  };
+
+  return (
+    <div className="App">
+      <span className="heading">ToDo</span>
+      <Input todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
+    </div>
+  );
+};
 
 export default App;
