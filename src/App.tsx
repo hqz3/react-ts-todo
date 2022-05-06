1d9673e26901401127b00dc738dc09ea864683e6
hqz3 <heqzheng@gmail.com>
2022-05-06 17:45:07 -0400

    Implement useReducer to handle task actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 // Component
 import Input from "./components/Input";
 import TodoList from "./components/TodoList";
 // Model
-import { Todo } from "./model";
+import { TodoReducer } from "./model";
 // Style
 import "./App.css";
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  const handleAdd = (e: React.FormEvent) => {
-    e.preventDefault();
-    if (todo) {
-      setTodos([
-        ...todos,
-        {
-          id: Date.now(),
-          todo,
-          isComplete: false,
-        },
-      ]);
-      setTodo("");
-    }
-  };
+  const [todos, dispatch] = useReducer(TodoReducer, []);
 
   return (
     <div className="App">
       <span className="heading">ToDo</span>
-      <Input todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
-      <TodoList todos={todos} setTodos={setTodos} />
+      <Input todo={todo} setTodo={setTodo} dispatch={dispatch} />
+      <TodoList todos={todos} dispatch={dispatch} />
     </div>
   );
 };
