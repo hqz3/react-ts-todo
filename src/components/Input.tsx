9aadcd6dfebb9ceeb156661d2dc5c55e5b9d990d
hqz3 <heqzheng@gmail.com>
2022-05-06 17:27:39 -0400

    Complete Input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,11 +1,37 @@
-import React from "react";
-// Styles
+import React, { useRef } from "react";
+// Model
+import { Actions } from "../model";
+// Style
 import "./styles.css";
 
-const Input = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  dispatch: React.Dispatch<Actions>;
+}
+
+const Input = ({ todo, setTodo, dispatch }: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form action="" className="input">
-      <input className="input-box" type="input" placeholder="Enter a task" />
+    <form
+      action=""
+      className="input"
+      onSubmit={(e) => {
+        e.preventDefault();
+        dispatch({ type: "add", payload: todo });
+        inputRef.current?.blur();
+        setTodo("");
+      }}
+    >
+      <input
+        className="input-box"
+        type="input"
+        placeholder="Enter a task"
+        value={todo}
+        onChange={(e) => setTodo(e.currentTarget.value)}
+        ref={inputRef}
+      />
       <button className="input-submit" type="submit">
         Add
       </button>
